167115d53e676d26c17be8865b20d63d35e7dacf
Matt Masurka <m.masurka@gmail.com>
2022-07-14 14:22:18 -0700

    initial contracts

---
+++ contracts/SoundNft/ERC721A/extensions/ERC721ABurnable.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+// ERC721A Contracts v4.1.0
+// Creator: Chiru Labs
+
+pragma solidity ^0.8.4;
+
+import './IERC721ABurnable.sol';
+import '../ERC721A.sol';
+
+/**
+ * @title ERC721A Burnable Token
+ * @dev ERC721A Token that can be irreversibly burned (destroyed).
+ */
+abstract contract ERC721ABurnable is ERC721A, IERC721ABurnable {
+    /**
+     * @dev Burns `tokenId`. See {ERC721A-_burn}.
+     *
+     * Requirements:
+     *
+     * - The caller must own `tokenId` or be an approved operator.
+     */
+    function burn(uint256 tokenId) public virtual override {
+        _burn(tokenId, true);
+    }
+}
