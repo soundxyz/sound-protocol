68dc840fcee729b72616b99f75697641e84d72ac
Matt Masurka <m.masurka@gmail.com>
2022-07-20 16:49:36 -0700

    Adds changesets  (#9)
    * Removes node_modules from git tracking
    
    * Adds danger
    
    * Adds changesets
    
    * Removes readme

---
+++ dangerfile.ts
@@ -0,0 +1,17 @@
+import { danger } from "danger";
+
+const CHANGESET_REGEX_CHECK = /^(.changeset)(\/\w+)/;
+const NO_CHANGESET_LABEL = "approved-no-changeset";
+
+const containsChangeset = danger.git.created_files.some((file) =>
+  CHANGESET_REGEX_CHECK.test(file)
+);
+
+if (!containsChangeset) {
+  const noChangesetNeeded = danger.github.issue.labels.some(
+    (label) => label.name === NO_CHANGESET_LABEL
+  );
+  if (!noChangesetNeeded) {
+    fail("This PR does not contain a changeset. Please add one.");
+  }
+}
