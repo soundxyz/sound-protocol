fe5a4f6edc9943a518d3f7e6315369148c157824
Matt Masurka <m.masurka@gmail.com>
2022-07-20 18:02:05 -0700

    SoundNft -> SoundEdition (#6)
    * Implements initialize in SoundNftV1
    
    * spec
    
    * SoundNft -> SoundEdition
    
    * README

---
+++ README.md
@@ -0,0 +1,3 @@
+# Sound Protocol 2.0
+
+See [spec](./spec.md) for current spec.
--- contracts/SoundEdition/ISoundEditionV1.sol
+++ contracts/SoundEdition/ISoundEditionV1.sol
@@ -2,8 +2,7 @@
 
 pragma solidity ^0.8.15;
 
-import "chiru-labs/ERC721A-Upgradeable/ERC721AUpgradeable.sol";
-import "openzeppelin-upgradeable/access/OwnableUpgradeable.sol";
+import "chiru-labs/ERC721A-Upgradeable/interfaces/IERC721AUpgradeable.sol";
 
 /*
                  ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒                
@@ -31,8 +30,12 @@ import "openzeppelin-upgradeable/access/OwnableUpgradeable.sol";
                ▓██████████████████████████████████████████████████████████                
 */
 
-/// @title ISoundNftV1
+/// @title ISoundEditionV1
 /// @author Sound.xyz
-interface ISoundNftV1 {
-  function initialize(string memory _name, string memory _symbol) external;
+interface ISoundEditionV1 is IERC721AUpgradeable {
+    function initialize(
+        address _owner,
+        string memory _name,
+        string memory _symbol
+    ) external;
 }
