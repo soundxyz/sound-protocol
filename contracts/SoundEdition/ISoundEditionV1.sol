1fa1b97db3281e786c7ef6b3b86b0fd338b21364
Vectorized <webby1111@hotmail.com>
2022-07-27 09:43:01 +0000

    Edit mint comments

--- contracts/SoundEdition/ISoundEditionV1.sol
+++ contracts/SoundEdition/ISoundEditionV1.sol
@@ -44,5 +44,12 @@ interface ISoundEditionV1 is IERC721AUpgradeable {
         string memory _contractURI
     ) external;
 
+    /// @notice Mints `_quantity` tokens to addrress `_to`
+    /// Each token will be assigned a token ID that is consecutively increasing.
+    /// The caller must have the `MINTER_ROLE`, which can be granted via
+    /// {grantRole}. Multiple minters, such as different minter contracts,
+    /// can be authorized simultaneously.
+    /// @param _to Address to mint to
+    /// @param _quantity Number of tokens to mint
     function mint(address _to, uint256 _quantity) external payable;
 }
--- contracts/SoundEdition/SoundEditionV1.sol
+++ contracts/SoundEdition/SoundEditionV1.sol
@@ -178,7 +178,10 @@ contract SoundEditionV1 is
     }
 
     /// @notice Mints `_quantity` tokens to addrress `_to`
-    /// Each token will be assigned a token ID that is consecutively increasing
+    /// Each token will be assigned a token ID that is consecutively increasing.
+    /// The caller must have the `MINTER_ROLE`, which can be granted via
+    /// {grantRole}. Multiple minters, such as different minter contracts,
+    /// can be authorized simultaneously.
     /// @param _to Address to mint to
     /// @param _quantity Number of tokens to mint
     function mint(address _to, uint256 _quantity) public payable onlyRole(MINTER_ROLE) {
