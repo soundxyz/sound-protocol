e3e547a06d5b7c457ff18044bceb484e730b624d
apoorvlathey <latheyapoorv@gmail.com>
2022-08-05 12:22:29 +0530

    add events

--- contracts/SoundFeeRegistry/SoundFeeRegistry.sol
+++ contracts/SoundFeeRegistry/SoundFeeRegistry.sol
@@ -7,14 +7,32 @@ import "openzeppelin/access/Ownable.sol";
 /// @title SoundFeeRegistry
 /// @author Sound.xyz
 contract SoundFeeRegistry is Ownable {
+    // ================================
+    // CONSTANTS
+    // ================================
+
     uint256 private constant MAX_BPS = 10_000;
+
+    // ================================
+    // STORAGE
+    // ================================
 
     address public soundFeeAddress;
 
     /// @dev platform fee in bps (0 to 10,000)
     uint32 public platformBPSFee;
 
+    // ================================
+    // EVENTS & ERRORS
+    // ================================
+    event SoundFeeAddressSet(address soundFeeAddress);
+    event PlatformFeeSet(uint32 platformBPSFee);
+
     error InvalidBPSFee();
+
+    // ================================
+    // PUBLIC & EXTERNAL WRITABLE FUNCTIONS
+    // ================================
 
     constructor(address soundFeeAddress_, uint32 platformBPSFee_) {
         soundFeeAddress = soundFeeAddress_;
@@ -25,12 +43,18 @@ contract SoundFeeRegistry is Ownable {
 
     function setSoundFeeAddress(address soundFeeAddress_) external onlyOwner {
         soundFeeAddress = soundFeeAddress_;
+        emit SoundFeeAddressSet(soundFeeAddress_);
     }
 
     function setPlatformBPSFee(uint32 platformBPSFee_) external onlyOwner {
         _verifyBPS(platformBPSFee_);
         platformBPSFee = platformBPSFee_;
+        emit PlatformFeeSet(platformBPSFee_);
     }
+
+    // ================================
+    // INTERNAL FUNCTIONS
+    // ================================
 
     function _verifyBPS(uint32 fee) internal pure {
         if (fee > MAX_BPS) revert InvalidBPSFee();
