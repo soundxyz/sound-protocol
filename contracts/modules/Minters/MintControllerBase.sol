3dd3368706706eca60bcdd849ce49b134c0cdccc
Vectorized <webby1111@hotmail.com>
2022-08-05 13:53:39 +0000

    Add ability to renouce mint controller access

--- contracts/modules/Minters/MintControllerBase.sol
+++ contracts/modules/Minters/MintControllerBase.sol
@@ -5,6 +5,11 @@ pragma solidity ^0.8.15;
 /// @title Mint Controller Base
 /// @dev The `MintControllerBase` class maintains a central storage record of mint controllers.
 abstract contract MintControllerBase {
+    struct ControllerData {
+        address addr;
+        bool access;
+    }
+
     /// @dev The caller must be the the controller of this edition to perform this action.
     error MintControllerUnauthorized();
 
@@ -14,17 +19,24 @@ abstract contract MintControllerBase {
     /// @dev A mint controller is already assigned to this edition.
     error MintControllerAlreadyExists(address controller);
 
+    /// @dev Emitted when the mint `controller` for `edition` renounces their own access.
+    event MintControllerAccessRenounced(address indexed edition, address indexed controller);
+
     /// @dev Emitted when the mint `controller` for `edition` is changed.
     event MintControllerUpdated(address indexed edition, address indexed controller);
 
     /// @dev Maps an edition to a controller.
-    mapping(address => address) private _controllers;
+    mapping(address => ControllerData) private _controllerData;
 
     /// @dev Restricts the function to be only callable by the controller of `edition`.
     modifier onlyEditionMintController(address edition) virtual {
-        address controller = _controllers[edition];
-        if (controller == address(0)) revert MintControllerNotFound();
-        if (msg.sender != controller) revert MintControllerUnauthorized();
+        ControllerData memory controllerData = _controllerData[edition];
+        address controller = controllerData.addr;
+        
+        if (controllerData.addr == address(0)) revert MintControllerNotFound();
+        if (msg.sender != controllerData.addr) revert MintControllerUnauthorized();
+        if (!controllerData.access) revert MintControllerUnauthorized();
+        
         _;
     }
 
@@ -32,8 +44,13 @@ abstract contract MintControllerBase {
     /// Calling conditions:
     /// - The `edition` must not have a controller.
     function _createEditionMintController(address edition) internal {
-        if (_controllers[edition] != address(0)) revert MintControllerAlreadyExists(_controllers[edition]);
-        _controllers[edition] = msg.sender;
+        ControllerData memory controllerData = _controllerData[edition];
+        address controller = controllerData.addr;
+        
+        if (controller != address(0)) revert MintControllerAlreadyExists(controller);
+        _controllerData[edition].addr = msg.sender;
+        _controllerData[edition].access = true;
+        
         emit MintControllerUpdated(edition, msg.sender);
     }
 
@@ -43,9 +60,14 @@ abstract contract MintControllerBase {
         setEditionMintController(edition, address(0));
     }
 
+    /// @dev Returns if the mint controller for `edition` has access.
+    function editionMintControllerHasAccess(address edition) public view returns (bool) {
+        return _controllerData[edition].access;
+    }
+
     /// @dev Returns the mint controller for `edition`.
     function editionMintController(address edition) public view returns (address) {
-        return _controllers[edition];
+        return _controllerData[edition].addr;
     }
 
     /// @dev Sets the new `controller` for `edition`.
@@ -56,7 +78,19 @@ abstract contract MintControllerBase {
         virtual
         onlyEditionMintController(edition)
     {
-        _controllers[edition] = controller;
+        _controllerData[edition].addr = controller;
         emit MintControllerUpdated(edition, controller);
+    }
+
+    /// @dev Sets the new `controller` for `edition`.
+    /// Calling conditions:
+    /// - The caller must be the current controller for `edition`.
+    function renounceEditionMintControllerAccess(address edition)
+        public
+        virtual
+        onlyEditionMintController(edition)
+    {
+        _controllerData[edition].access = false;
+        emit MintControllerAccessRenounced(edition, _controllerData[edition].addr);
     }
 }
