9ea343f591e03aa05ca32bf1da3876632159b418
Matt Masurka <m.masurka@gmail.com>
2022-08-10 12:51:30 -0700

    MockMinter.sol & MaxSupplyReached error (#33)
    * MockMinter.sol & MaxSupplyReached error
    
    * Implementing MockMinter
    
    * Removes commented test

---
+++ tests/mocks/MockMinter.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity ^0.8.15;
+
+import "../../contracts/modules/Minters/MintControllerBase.sol";
+
+contract MockMinter is MintControllerBase {
+    function createEditionMintController(address edition) external returns (uint256 mintId) {
+        mintId = _createEditionMintController(edition);
+    }
+
+    function deleteEditionMintController(address edition, uint256 mintId) external {
+        _deleteEditionMintController(edition, mintId);
+    }
+
+    function mint(
+        address edition,
+        uint256 mintId,
+        uint32 quantity,
+        uint256 price
+    ) external payable {
+        _mint(edition, mintId, msg.sender, quantity, quantity * price);
+    }
+}
