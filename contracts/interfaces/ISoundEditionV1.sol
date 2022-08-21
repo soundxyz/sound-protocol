2110bfb20c99871794b905db358997a24aa4c588
Vignesh Hirudayakanth <vigneshkanth@gmail.com>
2022-08-21 13:10:42 -0700

    immutables

--- contracts/interfaces/ISoundEditionV1.sol
+++ contracts/interfaces/ISoundEditionV1.sol
@@ -6,6 +6,7 @@ import { IERC721AUpgradeable } from "chiru-labs/ERC721A-Upgradeable/IERC721AUpgr
 import { IERC2981Upgradeable } from "openzeppelin-upgradeable/interfaces/IERC2981Upgradeable.sol";
 import { IERC165Upgradeable } from "openzeppelin-upgradeable/interfaces/IERC165Upgradeable.sol";
 import { ISoundEditionActions } from "./edition/ISoundEditionActions.sol";
+import { ISoundEditionImmutables } from "./edition/ISoundEditionImmutables.sol";
 import { ISoundEditionEventsAndErrors } from "./edition/ISoundEditionEventsAndErrors.sol";
 import { ISoundEditionOwnerActions } from "./edition/ISoundEditionOwnerActions.sol";
 import { ISoundEditionState } from "./edition/ISoundEditionState.sol";
@@ -16,19 +17,14 @@ import { IMetadataModule } from "./IMetadataModule.sol";
  * @author Sound.xyz
  */
 interface ISoundEditionV1 is
+    IERC721AUpgradeable,
+    IERC2981Upgradeable,
     ISoundEditionActions,
+    ISoundEditionImmutables,
     ISoundEditionEventsAndErrors,
     ISoundEditionOwnerActions,
-    ISoundEditionState,
-    IERC721AUpgradeable,
-    IERC2981Upgradeable
+    ISoundEditionState
 {
-    /// Getter for minter role hash
-    function MINTER_ROLE() external returns (bytes32);
-
-    /// Getter for admin role hash
-    function ADMIN_ROLE() external returns (bytes32);
-
     /**
      * @dev Informs other contracts which interfaces this contract supports.
      * https://eips.ethereum.org/EIPS/eip-165
