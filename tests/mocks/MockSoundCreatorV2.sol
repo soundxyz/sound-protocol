73b3e9a3addabd3662e309122dc6ad4e4ecc4e2f
Matt Masurka <m.masurka@gmail.com>
2022-08-22 10:53:42 -0700

    Implements UUPSUpgradeable on SoundCreatorV1 (#87)
    * Implements UUPSUpgradeable on SoundCreatorV1
    
    * Fixes tests & deploy script

---
+++ tests/mocks/MockSoundCreatorV2.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+pragma solidity ^0.8.16;
+
+import "openzeppelin-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+
+contract MockSoundCreatorV2 is UUPSUpgradeable {
+    function success() external pure returns (string memory) {
+        return "upgrade to v2 success!";
+    }
+
+    function _authorizeUpgrade(address) internal override {}
+}
