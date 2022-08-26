692a82d9619256e7b4789c2e8627663da8a20232
Matt Masurka <m.masurka@gmail.com>
2022-08-26 13:41:35 -0700

    supportsInterface fix

--- scripts/solidity/getInterfaceId.s.sol
+++ scripts/solidity/getInterfaceId.s.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.16;
 
 import { Script } from "forge-std/Script.sol";
 import "forge-std/console.sol";
+import { ISoundEditionV1 } from "@core/interfaces/ISoundEditionV1.sol";
 import { IMinterModule } from "@core/interfaces/IMinterModule.sol";
 import { IFixedPriceSignatureMinter } from "@modules/interfaces/IFixedPriceSignatureMinter.sol";
 import { IMerkleDropMinter } from "@modules/interfaces/IMerkleDropMinter.sol";
@@ -10,6 +11,9 @@ import { IRangeEditionMinter } from "@modules/interfaces/IRangeEditionMinter.sol
 
 contract GetInterfaceId is Script {
     function run() external {
+        console.log("ISoundEditionV1");
+        console.logBytes4(type(ISoundEditionV1).interfaceId);
+
         console.log("IMinterModule");
         console.logBytes4(type(IMinterModule).interfaceId);
 
