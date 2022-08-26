44a6db9340412002df9a35895c96af694349f9ea
Matt Masurka <m.masurka@gmail.com>
2022-08-26 11:33:07 -0700

    SdkTest script

--- scripts/solidity/deploy.s.sol
+++ scripts/solidity/deploy.s.sol
@@ -2,10 +2,13 @@
 pragma solidity ^0.8.16;
 
 import { Script } from "forge-std/Script.sol";
+import { ERC1967Proxy } from "openzeppelin/proxy/ERC1967/ERC1967Proxy.sol";
+
+import { ISoundFeeRegistry } from "@core/interfaces/ISoundFeeRegistry.sol";
 import { SoundEditionV1 } from "@core/SoundEditionV1.sol";
 import { SoundCreatorV1 } from "@core/SoundCreatorV1.sol";
 import { GoldenEggMetadata } from "@modules/GoldenEggMetadata.sol";
-import { FixedPricePermissionedSaleMinter } from "@modules/FixedPricePermissionedSaleMinter.sol";
+import { FixedPriceSignatureMinter } from "@modules/FixedPriceSignatureMinter.sol";
 import { MerkleDropMinter } from "@modules/MerkleDropMinter.sol";
 import { RangeEditionMinter } from "@modules/RangeEditionMinter.sol";
 
@@ -13,10 +16,13 @@ contract Deploy is Script {
     function run() external {
         vm.startBroadcast();
 
-        new GoldenEggMetadataModule();
-        new FixedPriceSignatureMinter();
-        new MerkleDropMinter();
-        new RangeEditionMinter();
+        // TODO - deploy this for real
+        ISoundFeeRegistry soundFeeRegistry = ISoundFeeRegistry(address(0));
+
+        new GoldenEggMetadata();
+        new FixedPriceSignatureMinter(soundFeeRegistry);
+        new MerkleDropMinter(soundFeeRegistry);
+        new RangeEditionMinter(soundFeeRegistry);
 
         // Deploy implementations
         SoundEditionV1 editionImplementation = new SoundEditionV1();
@@ -24,7 +30,7 @@ contract Deploy is Script {
 
         // Deploy creator proxy
         ERC1967Proxy proxy = new ERC1967Proxy(address(creatorImplementation), bytes(""));
-        soundCreator = SoundCreatorV1(address(proxy));
+        SoundCreatorV1 soundCreator = SoundCreatorV1(address(proxy));
 
         // Initialize creator
         soundCreator.initialize(address(editionImplementation));
