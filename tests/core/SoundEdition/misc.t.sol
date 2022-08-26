4ca84e60181ea1164d179e2345dabee8a25079a6
Matt Masurka <m.masurka@gmail.com>
2022-08-26 14:11:47 -0700

    supportsInterface fix (#120)

---
+++ tests/core/SoundEdition/misc.t.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity ^0.8.16;
+
+import { IAccessControlEnumerableUpgradeable } from "openzeppelin-upgradeable/access/IAccessControlEnumerableUpgradeable.sol";
+
+import { SoundEditionV1 } from "@core/SoundEditionV1.sol";
+import { ISoundEditionV1 } from "@core/interfaces/ISoundEditionV1.sol";
+import { TestConfig } from "../../TestConfig.sol";
+
+/**
+ * @dev Miscellaneous tests for SoundEdition
+ */
+contract SoundEdition_misc is TestConfig {
+    function test_supportsInterface() public {
+        SoundEditionV1 edition = createGenericEdition();
+        bool supportsEditionIface = edition.supportsInterface(type(ISoundEditionV1).interfaceId);
+        assertTrue(supportsEditionIface);
+    }
+}
