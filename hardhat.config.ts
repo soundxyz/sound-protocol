ac8c2cc15c94643c9a04464cf25411d941d7151f
Matt Masurka <m.masurka@gmail.com>
2022-08-27 23:19:51 -0700

    installs hardhat

---
+++ hardhat.config.ts
@@ -0,0 +1,25 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+import "@typechain/hardhat";
+import "@nomiclabs/hardhat-ethers";
+import "tsconfig-paths/register";
+
+const config: HardhatUserConfig = {
+    solidity: {
+        version: "0.8.16",
+        settings: {
+            optimizer: {
+                enabled: true,
+                runs: 200,
+            },
+        },
+    },
+    typechain: {
+        outDir: "./typechain",
+    },
+    paths: {
+        sources: "contracts/",
+    },
+};
+
+export default config;
