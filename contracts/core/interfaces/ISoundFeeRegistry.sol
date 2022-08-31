6049866f0697db14ecd63b8816a566203b90026b
Vectorized <webby1111@hotmail.com>
2022-08-31 12:28:49 +0800

    Cleanup pass (platformFee) (#146)
    * Add platformFee
    
    * Add test

--- contracts/core/interfaces/ISoundFeeRegistry.sol
+++ contracts/core/interfaces/ISoundFeeRegistry.sol
@@ -74,4 +74,11 @@ interface ISoundFeeRegistry {
      * @return The configured value.
      */
     function platformFeeBPS() external view returns (uint16);
+
+    /**
+     * @dev The platform fee for `requiredEtherValue`.
+     * @param requiredEtherValue The required Ether value for payment.
+     * @return fee The computed value.
+     */
+    function platformFee(uint128 requiredEtherValue) external view returns (uint128 fee);
 }
