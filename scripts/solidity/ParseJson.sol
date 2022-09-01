775399dc196a488647b0494263574043cff91e61
Matt Masurka <m.masurka@gmail.com>
2022-08-31 20:43:32 -0700

    Updating deploy script (#158)
    * Updating deploy script
    
    * Working as expected

--- scripts/solidity/ParseJson.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.16;
-
-interface ParseJson {
-    function parseJson(string calldata, string calldata) external returns (bytes memory);
-
-    function parseJson(string calldata) external returns (bytes memory);
-}
