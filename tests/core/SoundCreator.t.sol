ffa0118daebe9ee39dd67e5b5d4a8ed456a161b6
Vectorized <webby1111@hotmail.com>
2022-09-02 21:10:37 +0800

    Update tests/core/SoundCreator.t.sol
    Co-authored-by: Vignesh Hirudayakanth <vigneshkanth@gmail.com>

--- tests/core/SoundCreator.t.sol
+++ tests/core/SoundCreator.t.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity ^0.8.16;
 
-import "forge-std/console.sol";
 
 import { ERC1967Proxy } from "openzeppelin/proxy/ERC1967/ERC1967Proxy.sol";
 import { Clones } from "openzeppelin/proxy/Clones.sol";
