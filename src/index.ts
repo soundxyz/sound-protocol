4d12088d5f2495a7961c5d3d2005863f193af840
Matt Masurka <m.masurka@gmail.com>
2022-09-06 16:41:06 -0700

    camelCase

--- src/index.ts
+++ src/index.ts
@@ -4,33 +4,33 @@ import previewDeployment from "./preview.json";
 
 const contractAddresses = {
     staging: {
-        SoundCreatorV1: stagingDeployment.transactions.find((tx) => tx.contractName === "SoundCreatorV1")
+        soundCreatorV1: stagingDeployment.transactions.find((tx) => tx.contractName === "SoundCreatorV1")
             ?.contractAddress,
-        FixedPriceSignatureMinter: stagingDeployment.transactions.find(
+        fixedPriceSignatureMinter: stagingDeployment.transactions.find(
             (tx) => tx.contractName === "FixedPriceSignatureMinter"
         )?.contractAddress,
-        RangeEditionMinter: stagingDeployment.transactions.find((tx) => tx.contractName === "RangeEditionMinter")
+        rangeEditionMinter: stagingDeployment.transactions.find((tx) => tx.contractName === "RangeEditionMinter")
             ?.contractAddress,
-        MerkleDropMinter: stagingDeployment.transactions.find((tx) => tx.contractName === "MerkleDropMinter")
+        merkleDropMinter: stagingDeployment.transactions.find((tx) => tx.contractName === "MerkleDropMinter")
             ?.contractAddress,
-        GoldenEggMetadata: stagingDeployment.transactions.find((tx) => tx.contractName === "GoldenEggMetadata")
+        goldenEggMetadata: stagingDeployment.transactions.find((tx) => tx.contractName === "GoldenEggMetadata")
             ?.contractAddress,
-        SoundFeeRegistry: stagingDeployment.transactions.find((tx) => tx.contractName === "SoundFeeRegistry")
+        soundFeeRegistry: stagingDeployment.transactions.find((tx) => tx.contractName === "SoundFeeRegistry")
             ?.contractAddress,
     },
     preview: {
-        SoundCreatorV1: previewDeployment.transactions.find((tx) => tx.contractName === "SoundCreatorV1")
+        soundCreatorV1: previewDeployment.transactions.find((tx) => tx.contractName === "SoundCreatorV1")
             ?.contractAddress,
-        FixedPriceSignatureMinter: previewDeployment.transactions.find(
+        fixedPriceSignatureMinter: previewDeployment.transactions.find(
             (tx) => tx.contractName === "FixedPriceSignatureMinter"
         )?.contractAddress,
-        RangeEditionMinter: previewDeployment.transactions.find((tx) => tx.contractName === "RangeEditionMinter")
+        rangeEditionMinter: previewDeployment.transactions.find((tx) => tx.contractName === "RangeEditionMinter")
             ?.contractAddress,
-        MerkleDropMinter: previewDeployment.transactions.find((tx) => tx.contractName === "MerkleDropMinter")
+        merkleDropMinter: previewDeployment.transactions.find((tx) => tx.contractName === "MerkleDropMinter")
             ?.contractAddress,
-        GoldenEggMetadata: previewDeployment.transactions.find((tx) => tx.contractName === "GoldenEggMetadata")
+        goldenEggMetadata: previewDeployment.transactions.find((tx) => tx.contractName === "GoldenEggMetadata")
             ?.contractAddress,
-        SoundFeeRegistry: previewDeployment.transactions.find((tx) => tx.contractName === "SoundFeeRegistry")
+        soundFeeRegistry: previewDeployment.transactions.find((tx) => tx.contractName === "SoundFeeRegistry")
             ?.contractAddress,
     },
 };
