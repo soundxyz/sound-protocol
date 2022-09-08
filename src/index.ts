a9654fa8bb05c97dd055fa04b38a17f519e5ab8a
Matt Masurka <m.masurka@gmail.com>
2022-09-08 16:49:24 -0700

    builds interfaceIds.ts

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,10 @@
-import ids from "./json/interfaceIds.json";
 import staging from "./json/staging.json";
 import preview from "./json/preview.json";
-
-// TODO: figure out how we can generate this file. Importing the JSON
-// is not ideal because typescript just shows them as `string`. We need
-// interfaceIds as constants for the SDK to know which minter to select from.
+import { interfaceIds } from "./interfaceIds.js";
 
 const contractAddresses = {
     staging,
     preview,
-} as const;
-
-const interfaceIds = {
-    ISoundEditionV1: "0x183e0a77",
-    IMinterModule: "0x37c74bd8",
-    IFixedPriceSignatureMinter: "0x110099e0",
-    IMerkleDropMinter: "0x84b6980c",
-    IRangeEditionMinter: "0xc73d6ffa",
 } as const;
 
 export { interfaceIds, contractAddresses };
---
+++ src/interfaceIds.ts
@@ -0,0 +1,7 @@
+export const interfaceIds = {
+    ISoundEditionV1: "0x183e0a77",
+    IMinterModule: "0x37c74bd8",
+    IFixedPriceSignatureMinter: "0x110099e0",
+    IMerkleDropMinter: "0x84b6980c",
+    IRangeEditionMinter: "0xc73d6ffa",
+} as const;
