531b3f32dcb49c3352f7059da79633a85e4e0577
Matt Masurka <m.masurka@gmail.com>
2022-09-16 11:40:56 -0700

    Fixes scripts for preparing release (#224)

--- script/js/buildAddressJsonFile.ts
+++ script/js/buildAddressJsonFile.ts
@@ -45,16 +45,4 @@ async function buildAddressJsonFile() {
     }
 }
 
-/**
- * Builds an interfaceIds.js file so we can publish the ids as string constants.
- */
-async function buildInterfaceIdsFile() {
-    const json = await readFile(`src/json/interfaceIds.json`, "utf8");
-
-    const filePath = `src/interfaceIds.ts`;
-    await ensureFile(filePath);
-    await writeFile(filePath, `export const interfaceIds = ${JSON.stringify(JSON.parse(json))} as const`, "utf-8");
-}
-
 await buildAddressJsonFile();
-await buildInterfaceIdsFile();
