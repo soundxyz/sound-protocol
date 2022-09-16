6361f1fd86c844530787d768ae64a1e7410beb76
Vignesh Hirudayakanth <vigneshkanth@gmail.com>
2022-09-16 05:22:25 -0700

    InterfaceId update

--- src/interfaceIds.ts
+++ src/interfaceIds.ts
@@ -1,7 +1,8 @@
 export const interfaceIds = {
-    ISoundEditionV1: "0x98b4ce7c",
+    ISoundEditionV1: "0x97f5992c",
     IMinterModule: "0x37c74bd8",
-    IFixedPriceSignatureMinter: "0x110099e0",
-    IMerkleDropMinter: "0x84b6980c",
-    IRangeEditionMinter: "0xc73d6ffa",
+    IFixedPriceSignatureMinter: "0xa61bd96f",
+    IMerkleDropMinter: "0xd8eafa50",
+    IPublicSaleMinter: "0xa7ea8688",
+    IRangeEditionMinter: "0x4d4a2e35",
 } as const;
