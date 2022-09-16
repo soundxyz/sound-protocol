2e90d28de07e114ffefcb23781f9f01053c8cf39
Vignesh Hirudayakanth <vigneshkanth@gmail.com>
2022-09-16 08:12:47 -1000

    Update interfaces (#221)

--- src/interfaceIds.ts
+++ src/interfaceIds.ts
@@ -1,8 +1,8 @@
 export const interfaceIds = {
-    ISoundEditionV1: "0x97f5992c",
+    ISoundEditionV1: "0xa8c2aeec",
     IMinterModule: "0x37c74bd8",
     IFixedPriceSignatureMinter: "0xa61bd96f",
     IMerkleDropMinter: "0xd8eafa50",
-    IPublicSaleMinter: "0xa7ea8688",
+    IEditionMaxMinter: "0xa7ea8688",
     IRangeEditionMinter: "0x4d4a2e35",
 } as const;
