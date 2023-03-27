1f49dd5922c87c34d74f11bacfd696c2f8a07ad4
Pablo Sáez <pablosaez1995@gmail.com>
2023-03-27 14:55:38 -0400

    interface build changes

--- src/interfaceIds.ts
+++ src/interfaceIds.ts
@@ -1,6 +1,6 @@
 export const interfaceIds = {
     ISoundEditionV1: "0x50899e54",
-    ISoundEditionV1_1: "0x425aac3d",
+    ISoundEditionV1_2: "0xa176eca6",
     IMinterModule: "0x37c74bd8",
     IFixedPriceSignatureMinter: "0xa61bd96f",
     IMerkleDropMinter: "0x89691c4c",
